4d32503bb3c179cd43afc5d27c102c8b66a25622
MayraFazzari <mayrafazzariclerici.01@gmail.com>
2025-07-07 14:50:19 -0300

    filtros

--- Taller2/src/app/public/productos/productos.component.ts
+++ Taller2/src/app/public/productos/productos.component.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-productos',
-  standalone: true, // <-- importante
+  standalone: true,
   imports: [CommonModule, HttpClientModule],
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
+
 export class ProductosComponent implements OnInit {
   productos: any[] = [];
+  productosFiltrados: any[] = [];
+  categoriasSeleccionadas: Set<string> = new Set();
 
   constructor(private http: HttpClient) {}
 
@@ -18,6 +21,46 @@ export class ProductosComponent implements OnInit {
     this.http.get<any[]>('http://localhost:5000/productos')
       .subscribe(data => {
         this.productos = data;
+        this.productosFiltrados = data;  // Mostrar todos al inicio
       });
   }
+
+  filtrarPorCategoria(categoria: string, checked: boolean): void {
+    if (checked) {
+      this.categoriasSeleccionadas.add(categoria);
+    } else {
+      this.categoriasSeleccionadas.delete(categoria);
+    }
+
+    if (this.categoriasSeleccionadas.size === 0) {
+      this.productosFiltrados = this.productos;
+    } else {
+      this.productosFiltrados = this.productos.filter(p =>
+        this.categoriasSeleccionadas.has(p.clasificacion.toLowerCase())
+      );
+    }
+  }
+
+  onCategoriaChange(event: Event, categoria: string): void {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.filtrarPorCategoria(categoria, checked);
+  }
+
+  ordenSeleccionado: string = '';
+
+  ordenarProductos(orden: string): void {
+  this.ordenSeleccionado = orden;
+
+  this.productosFiltrados.sort((a, b) => {
+    if (orden === 'menor') {
+      return a.precio - b.precio;
+    } else if (orden === 'mayor') {
+      return b.precio - a.precio;
+    }
+    return 0;
+  });
 }
+
+}
+
+
